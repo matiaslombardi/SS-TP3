8f0222d14381bf723a7e80ea87bc090cc00efb61
srosati <srosati@itba.edu.ar>
2022-09-18 22:42:39 -0300

    Feat: slit_size param

--- run_fp.sh
+++ run_fp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-N=(50 100 150)
-SLIT_SIZES=(0.01)
+N=(100)
+SLIT_SIZES=(0.01 0.03 0.05)
 
 rm graphics/fps.txt
 
 for n in ${N[@]}; do
     for slit_size in ${SLIT_SIZES[@]}; do
         printf "${n} ${slit_size}\n" >> graphics/fps.txt
-        /usr/bin/env /Users/srosati/Library/Java/JavaVirtualMachines/openjdk-18.0.2.1/Contents/Home/bin/java --enable-preview -XX:+ShowCodeDetailsInExceptionMessages -cp /Users/srosati/Library/Application\ Support/Code/User/workspaceStorage/0cbe8661bfbfecf259f025c067e12e3b/redhat.java/jdt_ws/SS-TP3_3fa4554a/bin main.java.ar.edu.itba.ss.EventBasedSystem ${n} 0.01
+        /usr/bin/env /Users/srosati/Library/Java/JavaVirtualMachines/openjdk-18.0.2.1/Contents/Home/bin/java --enable-preview -XX:+ShowCodeDetailsInExceptionMessages -cp /Users/srosati/Library/Application\ Support/Code/User/workspaceStorage/0cbe8661bfbfecf259f025c067e12e3b/redhat.java/jdt_ws/SS-TP3_3fa4554a/bin main.java.ar.edu.itba.ss.EventBasedSystem ${n} ${slit_size} 0.001
         cd graphics
         python3 fp.py
         cd ..
--- src/main/java/ar/edu/itba/ss/EventBasedSystem.java
+++ src/main/java/ar/edu/itba/ss/EventBasedSystem.java
@@ -14,20 +14,20 @@ import java.util.stream.Collectors;
 public class EventBasedSystem {
     public static void main(String[] args) {
         System.out.println("Running");
-        if (args.length != 2) {
-            System.out.println("Usage: java EventBasedSystem <particle_count> <epsilon>");
+        if (args.length != 3) {
+            System.out.println("Usage: java EventBasedSystem <particle_count> <slit> <epsilon>");
             System.exit(1);
         }
 
         int particleAmount = Integer.parseInt(args[0]);
-        double fpEpsilon = Double.parseDouble(args[1]);
+        double slit = Double.parseDouble(args[1]);
+        double fpEpsilon = Double.parseDouble(args[2]);
 
         double speed = 0.01;
         double mass = 1;
         double radius = 0.0015;
         double height = 0.09;
         double width = 0.24;
-        double slit = 0.01;
 
         List<Particle> particles = ParticleGenerator.generate("static.txt", particleAmount,
                 height, width / 2, speed, mass, radius);
